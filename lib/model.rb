d4744726b61858b3e6c3105dfed4b927daee6fc3
André Gonçalves <andre.rigon@gmail.com>
2015-10-17 20:59:15 -0300

    verifiying signed api response

--- lib/model.rb
+++ lib/model.rb
@@ -17,11 +17,11 @@ module APIConfig
     Time.now.getutc.to_i
   end
 
-  def apiKey
+  def api_key
     'b07a12df7d52e6c118e5d47d3f9e60135b109a1f'
   end
 
-  def apiUri
+  def api_uri
     'http://api.fyber.com/feed/v1/offers.json'
   end
 end
@@ -40,12 +40,27 @@ class FyberRequest
   def to_url
     options = @config.merge(:timestamp => timestamp).sort.map { |k, v| "#{k}=#{v}" }.join('&')
     hash = generate_hash(options)
-    "#{apiUri}?#{options}&hashkey=#{hash}".to_s
+    "#{api_uri}?#{options}&hashkey=#{hash}".to_s
   end
 
   def generate_hash(options)
-    Digest::SHA1.hexdigest options + "&" + apiKey
+    Digest::SHA1.hexdigest options + "&" + api_key
   end
 end
 
+class FyberResponse
+  include APIConfig
 
+  def initialize(http_response)
+    raise RuntimeError, 'invalid API response' unless valid?(http_response)
+  end
+
+  def valid?(http_response)
+    hash = Digest::SHA1.hexdigest(http_response.body + api_key)
+    hash == http_response.headers[:'X-Sponsorpay-Response-Signature']
+  end
+
+
+
+
+end
--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -21,6 +21,30 @@ RSpec.describe 'models', '#to_url' do
       end
     end
 
+    describe "FyberResponse" do
+
+      it 'verifies correct response' do
+        http_response = double('http response')
+        allow(http_response).to receive(:body) { '{"foo" : "bar"}' }
+        allow(http_response).to receive(:headers) do
+          hash = Digest::SHA1.hexdigest(http_response.body + 'b07a12df7d52e6c118e5d47d3f9e60135b109a1f')
+          {:'X-Sponsorpay-Response-Signature' => hash }
+        end
+      end
+
+      it 'throw exception for invalid response' do
+        http_response = double('http response')
+        allow(http_response).to receive(:body) { '{"foo" : "bar"}' }
+        allow(http_response).to receive(:headers) do
+          hash = Digest::SHA1.hexdigest(http_response.body + 'wrong key')
+          {:'X-Sponsorpay-Response-Signature' => hash }
+        end
+
+        expect { FyberResponse.new(http_response) }.to raise_error(RuntimeError, 'invalid API response')
+      end
+
+    end
+
 end
 
 
